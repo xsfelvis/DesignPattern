e0365aa0307f51e720aba4700e21675768d29c4e
uestc_xsf <xsf_uestc_ncl@163.com>
2016-11-18 23:49:29 +0800

    :art: 增加新的单例功能

---
+++ app/src/main/java/xsf/design/Singleton/BetterSingleton.java
@@ -0,0 +1,10 @@
+package xsf.design.Singleton;
+
+/**
+ * Author: hzxushangfei
+ * Time: created at 2016/11/18.
+ * Copyright 2016 Netease. All rights reserved.
+ */
+
+public class BetterSingleton {
+}
---
+++ app/src/main/java/xsf/design/Singleton/SingletonManager.java
@@ -0,0 +1,10 @@
+package xsf.design.Singleton;
+
+/**
+ * Author: 彩笔学长
+ * Time: created at 2016/11/18.
+ * Description:
+ */
+
+public class SingletonManager {
+}
