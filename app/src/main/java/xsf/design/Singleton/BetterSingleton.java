8c9f922bae4896253939653a7d2f3e77d76dcdd1
uestc_xsf <xsf_uestc_ncl@163.com>
2016-11-18 23:50:28 +0800

    :wrench: 添加配置文件

--- app/src/main/java/xsf/design/Singleton/BetterSingleton.java
+++ app/src/main/java/xsf/design/Singleton/BetterSingleton.java
@@ -3,8 +3,19 @@ package xsf.design.Singleton;
 /**
  * Author: hzxushangfei
  * Time: created at 2016/11/18.
- * Copyright 2016 Netease. All rights reserved.
+ * Description:更加优雅的单例
  */
 
 public class BetterSingleton {
+    public BetterSingleton() {
+    }
+
+    public static BetterSingleton getInstance() {
+        return SingletonHolder.instance;
+    }
+
+    private static class SingletonHolder {
+        private static final BetterSingleton instance = new BetterSingleton();
+    }
+
 }
